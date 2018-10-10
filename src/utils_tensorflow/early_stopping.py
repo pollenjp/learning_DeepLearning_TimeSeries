c96b730b4eaf63d11100c965662f321f52c670b9
pollenJP <polleninjp@gmail.com>
2018-10-10 16:14:31 +0000

    Add early_stopping.py for TesnorFlow

---
+++ src/utils_tensorflow/early_stopping.py
@@ -0,0 +1,24 @@
+
+class EarlyStopping():
+    """
+    patience : int
+    verbose  : int, {0,1}
+    """
+    def __init__(self, patience=0, verbose=0):
+        self._step = 0
+        self._loss = float('inf')
+        self.patience = patience
+        self.verbose = verbose
+
+    def validate(self, loss):
+        if self._loss < loss:
+            self._step += 1
+            if self._step > self.patience:
+                if self.verbose:
+                    print('early stopping')
+                return True
+        else:
+            self._step = 0
+            self._loss = loss
+        return False
+
